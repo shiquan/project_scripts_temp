49a1ded7e16d3d6c09b6593057bd74dbe380e0ed
Shi Quan <shiquan.cn@gmail.com>
2017-02-13 09:30:02 +0800

    modified:   include/sequence.h 	modified:   lib/hgvs.c

--- lib/hgvs.c
+++ lib/hgvs.c
@@ -8,10 +8,10 @@ struct hgvs_spec {
 
 void hgvs_core_clear(struct hgvs_core *core)
 {
-    if ( core->name.name )
-        free(core->name.name);
     if ( core->name.name1 )
         free(core->name.name1);
+    if ( core->name.name2 )
+        free(core->name.name2);
     memset(core, 0, sizeof(struct hgvs_core));
 }
 void hgvs_des_clear(struct hgvs_des *des)
@@ -53,6 +53,34 @@ int parse_hgvs_name(char *name)
 
     return 0;
 }
+int find_the_block(struct genepred_line *line, int *blk_start, int *blk_end, int pos)
+{
+}
+// return 0 on success, 1 on out of range.
+int generate_hgvs_core(struct genepred_line *line, struct hgvs_core *name, int start, int end)
+{
+    int i;
+    int blk_start = 0, blk_end = 0;
+    // Locate start.
+    for (i = 0; i < line->exoncount; ++i) {
+        if ( find_the_block(line, &blk_start, &blk_end, start ) )
+            return 1;
+        
+    }
+    // Locate end. For most cases variants are snps, start == end.
+    if ( end != start ) {
+        for ( i = 0; i < line->exoncount; ++i ) {
+         if ( find_the_block(line, &blk_start, &blk_end, end ) )
+             return 1;
+
+         
+        }
+    }
+    name->name1 = strdup(line->name1);
+    name->name2 = strdup(line->name2);
+        
+    return 0;
+}
 // Fill all possible HGVS names for this variant.
 int fill_hgvs_name()
 {
@@ -61,11 +89,26 @@ int fill_hgvs_name()
     if ( des->start == 0 )
         error("Variant position is not inited.");
 
-    struct 
-    
-        
-}
+    struct genepred_line *line = genepred_retrieve_region(spec->data, des->name, des->start, des->end);
 
+    for ( ;; ) {
+        if ( line == NULL )
+            break;
+        
+        if ( des->l == des->m ) {
+            des->m += 2;
+            des->a = (struct hgvs_core*)realloc(des->a, des->m*sizeof(struct hgvs_core));
+        }
+
+        hgvs_core_clear(&des->a[des->l]);
+        if ( generate_hgvs_core(line, &des->a[des->l]) == 0 )
+            des->l ++;
+        struct genepred_line * temp = line;
+        line = line->next;
+        genepred_line_destroy(temp);
+    }
+    return 0;
+}
 
 #ifdef HGVS_MAIN
 int usage()
