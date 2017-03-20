2fcc8863a51b0091353203f2d6710632540d793c
Shi Quan <shiquan.cn@gmail.com>
2017-03-20 09:30:39 +0800

    new file:   include/hgvs_vcf.h 	new file:   lib/hgvs_vcf.c

---
+++ include/hgvs_vcf.h
@@ -0,0 +1,16 @@
+#ifndef C_HGVS_VCF_HEADER
+#define C_HGVS_VCF_HEADER
+#include "utils.h"
+#include "htslib/vcf.h"
+
+
+extern int init_hgvs_anno();
+extern int close_hgvs_anno();
+extern int setter_hgvs_vcf(bcf_hdr_t *hdr, bcf1_t *line);
+
+
+
+
+
+
+#endif 
