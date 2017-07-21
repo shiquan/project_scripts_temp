c9b276a2d6cb178b76883fab68a91aa9a0c44230
Shi Quan <shiquan.cn@gmail.com>
2017-07-21 12:58:38 +0800

    modified:   projects/bam/parse_UID_tag.c

--- projects/bam/parse_UID_tag.c
+++ projects/bam/parse_UID_tag.c
@@ -5,12 +5,14 @@
 // #include "htslib/hts.h"
 #include <unistd.h>
 #include "pkg_version.h"
+#include "number.h"
 
 int usage()
 {
     fprintf(stderr,
             "Usage: sam_parse_uid in.bam\n"
             "   -tag BC    barcode tag for sam file\n"
+            "   -len INT   barcode length capped to this value\n"
             "\n"
             "Version: %s\n"
             "Homepage: https://github.com/shiquan/small_projects\n"
@@ -24,15 +26,18 @@ struct args {
     samFile *in;
     bam_hdr_t *header;
     const char *bc_tag;
+    int length;
 } args = {
     .in = NULL,
     .header = NULL,
     .bc_tag = NULL,
+    .length = 0,
 };
 int parse_args(int argc, char **argv)
 {
     int i;
     const char *fn = NULL;
+    const char *length = NULL;
     for ( i = 1; i < argc; ) {
         const char *a = argv[i++];
         if ( strcmp(a, "-h") == 0 )
@@ -41,7 +46,9 @@ int parse_args(int argc, char **argv)
         const char **var = 0;
         if ( strcmp(a, "-tag") == 0 && args.bc_tag == NULL )
             var = &args.bc_tag;
-
+        else if ( strcmp(a, "-len") == 0 && args.length == 0 )
+            var = &length;
+                  
         if ( var != 0 ) {
             if ( i == argc) {
                 error_print("Miss an argument after %s.", a);
@@ -59,7 +66,10 @@ int parse_args(int argc, char **argv)
     if ( args.bc_tag == NULL )
         args.bc_tag = "BC";
     // todo: check the selfdefined tag is properly designed.
-        
+
+    if ( length )
+        args.length = str2int((char*)length);
+    
     if ( fn == NULL && !isatty(fileno(stdin)) )
         args.in =sam_open("-", "r");
     else if ( argc == 2 )
@@ -109,7 +119,9 @@ int sam_parse_UID()
                 string.l -= l;
                 kputc('\t', &string);
                 kputsn((char*)args.bc_tag, 2, &string); kputs(":Z:", &string);
-                kputs(s, &string);
+                if ( args.length && l > args.length )
+                    l = args.length;
+                kputsn(s, l, &string);
                 break;
             }
         }
